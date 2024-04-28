b95ba64c8cb0057759e802687773a898a8de78c6
Georgi <Georgiivanover@gmail.com>
2024-04-28 13:05:37 +0300

    Add project files.

---
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuotesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+
+    }
+}
