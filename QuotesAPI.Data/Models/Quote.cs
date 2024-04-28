044a0e06fe9ce325e74ebcc4a2f78090f00f5d14
Georgi <Georgiivanover@gmail.com>
2024-04-28 14:18:39 +0300

    Added DbContext

---
+++ QuotesAPI.Data/Models/Quote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotesAPI.Data.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+    }
+}
--- QuotesAPI/Program.cs
+++ QuotesAPI/Program.cs
@@ -1,4 +1,5 @@
-
+using QuotesAPI.Data;
+using Microsoft.EntityFrameworkCore;
 namespace QuotesAPI
 {
     public class Program
@@ -6,9 +7,12 @@ namespace QuotesAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
+            // Add services to the container.        
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {           
+                options.UseSqlServer(connectionString);
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
